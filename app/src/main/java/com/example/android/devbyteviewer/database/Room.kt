02ad988fb4b1ceb3ec4e18ea640c49c7b555a7d6
Daniel Valencia <dfvalenciav@gmail.com>
2021-11-05 20:25:11 -0500

    commit V1.3 (Setting Room Database)

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,16 +17,35 @@
 
 package com.example.android.devbyteviewer.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
 @Dao
 interface VideoDao {
     @Query("select * from databasevideo")
-    fun getVideos(): List<DatabaseVideo>
+    fun getVideos(): LiveData<List<DatabaseVideo>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase: RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+
+//CREATE DATABASE
+fun getDatabase(context: Context): VideosDatabase {
+    // initialize instance safelly using syncrhonized
+    synchronized(VideosDatabase::class.java){
+    if(!::INSTANCE.isInitialized){
+        INSTANCE = Room.databaseBuilder(context.applicationContext,
+                VideosDatabase::class.java,"videos").build()
+        }
+    }
+    return INSTANCE
 }
