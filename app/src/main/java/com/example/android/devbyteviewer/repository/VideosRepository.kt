f1553447b4d5a3fecee4d1f8d8091493d7834f95
Daniel Valencia <dfvalenciav@gmail.com>
2021-11-05 21:34:40 -0500

    commit V1.4 (Creating a repository with dependency injection)

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,42 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import com.example.android.devbyteviewer.network.NetworkVideoContainer
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+//Repository for fetching daevbyte videos from the network and storing them on the disk
+//Simple way of dependency injection
+
+class VideosRepository(private val database: VideosDatabase) {
+    // A playlist of videos that can be shown on the screen
+    // transformartion.map keeps convert one liveData to another
+    val videos : LiveData<List<Video>> = Transformations.map(database.videoDao.getVideos()){
+        it.asDomainModel()// it help us to convert databasevide to video
+    }
+
+    //refreshing videos
+    suspend fun refreshVideos (){
+        withContext(Dispatchers.IO){
+            /**
+             * Refresh the videos stored in the offline cache.
+             *
+             * This function uses the IO dispatcher to ensure the database insert database operation
+             * happens on the IO dispatcher. By switching to the IO dispatcher using `withContext` this
+             * function is now safe to call from any thread including the Main thread.
+             *
+             * To actually load the videos for use, observe [videos]
+             */
+            val playlist = Network.devbytes.getPlaylist().await()
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+        }
+    }
+}
